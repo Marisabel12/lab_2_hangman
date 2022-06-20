df13a7c63541ba0d2a6a5b77e1282900074499fe
marisabel flores kacka <floreskackamarisabel@gmail.com>
2022-06-20 11:51:24 -0400

    hangman

---
+++ src/hangman/components/DrawComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+//import { useKeyBoard } from "../hooks/useKeyBoard"; 
+
+export const DrawComponent = () => {
+    return (
+        <div>
+            <img src ={`/assets/img6.png`} alt="test" />
+        </div>
+    );
+};
---
+++ src/hangman/components/ItemKeyComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useKeyBoard } from "../hooks/useKeyBoard";
+
+export const ItemKeyComponent = () => {
+    const [keyState, setKeyState] = useKeyBoard();
+    const handlerClick = (key) => {
+        const newState = keyState.map((item) => {
+        if(key === item.key) {
+            item.state = true;
+        }
+        return item;
+    });
+    setKeyState(newState);
+};
+return (
+    <div className="flex-container">
+        {keyState.map(({ key, state }) => (
+            <ItemKeyComponent 
+            key={key}
+            mykey={state}
+            handlerClick={handlerClick}
+            />
+        ))}
+    </div>
+);
+}
